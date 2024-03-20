e3708b75b068662627d4dc7388ec73116e95e04c
Erwann <erwann.masson@datadoghq.com>
2024-03-20 23:10:04 +0100

    base code structure

---
+++ ctfdpy/ctfdpy/ctfd.py
@@ -0,0 +1,6 @@
+from .http import HTTPClient
+from .members import Members
+
+class CTFDClient:
+    def __init__(self) -> None:
+        raise NotImplementedError("CTFDClient is not implemented yet")
