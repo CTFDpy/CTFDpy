459a2cdf92c4ee60b71b6c8c17967225d1a787c1
Anemys <abdelmalik_kerbadou@yahoo.fr>
2024-04-07 01:26:57 +0200

    ✨ Add HTTPClient
    Co-authored-by: Erwann Masson <erwann.masson@gmail.com>

--- ctfdpy/ctfdpy/ctfd.py
+++ ctfdpy/ctfdpy/ctfd.py
@@ -1,6 +1,7 @@
 from .http import HTTPClient
 from .members import Members
 
+
 class CTFDClient:
-    def __init__(self) -> None:
-        raise NotImplementedError("CTFDClient is not implemented yet")
+    def __init__(self, url: str, token: str) -> None:
+        self.http = HTTPClient(url, token)
--- ctfdpy/ctfdpy/http.py
+++ ctfdpy/ctfdpy/http.py
@@ -1,3 +1,77 @@
+from enum import Enum
+from json import JSONDecodeError
+from typing import Any
+
+import httpx
+
+
+class CTFDError(Exception):
+    """Exception class to handle exceptions thrown for any request regarding CTFd API"""
+
+
+class HTTPMethod(Enum):
+    GET = "GET"
+    POST = "POST"
+    PATCH = "PATCH"
+    DELETE = "DELETE"
+    HEAD = "HEAD"
+
+
 class HTTPClient:
-    def __init__(self) -> None:
-        raise NotImplementedError("HTTPClient is not implemented yet")
+    def __init__(
+        self,
+        url: str,
+        token: str,
+    ) -> None:
+        self.client = httpx.Client(
+            base_url=url,
+            headers={
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+                "Authorization": f"Token {token}",
+            },
+        )
+
+    def _request(
+        self,
+        endpoint: str,
+        method: HTTPMethod,
+        params: dict[str, Any] | None = None,
+        json: dict[str, Any] = None,
+    ) -> dict[str, Any] | None:
+        try:
+            response = self.client.request(
+                method=method.value, url=endpoint, params=params, json=json
+            )
+
+            if not response.content:
+                return None
+
+            return response.json()
+        except httpx.RequestError as request_exc:
+            raise CTFDError(
+                f"An error occurred while requesting {request_exc.request.url!r}:\n{request_exc!r}"
+            )
+        except (JSONDecodeError, TypeError) as decode_exc:
+            raise CTFDError(f"The response could not be parsed:\n{decode_exc!r}")
+
+    def get_item(self, endpoint: str, id: int) -> dict[str, Any] | None:
+        return self._request(f"{endpoint}/{id}")
+
+    def get_items(self, endpoint: str) -> list[dict[str, Any]] | None:
+        data = self._request(endpoint, HTTPMethod.GET)
+        if not data:
+            return None
+
+        try:
+            if data["meta"]["pagination"]["pages"] > 1:
+                for i in range(1, data["meta"]["pagination"]["pages"] + 1):
+                    new_rq = self._request(endpoint, HTTPMethod.GET, params={"page": i})
+                    if not new_rq:
+                        continue
+
+                    data += new_rq
+        except KeyError:
+            pass
+
+        return data
