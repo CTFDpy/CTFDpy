a8888a78f00b40f6eda4d2feff18f51cf5c5b1b8
Erwann <erwann.masson@datadoghq.com>
2024-04-26 10:52:09 +0200

    team data

--- ctfdpy/ctfdpy/models/data.py
+++ ctfdpy/ctfdpy/models/data.py
@@ -15,7 +15,7 @@ class UserData:
     country: str = ""
     bracket_id: int = 0
     hidden: bool = True
-    banned: bool = True
+    banned: bool = False
     verified: bool = True
     language: str = ""
     created: dt| None = None
@@ -23,3 +23,24 @@ class UserData:
     team_id: int = 0
     place: int = 0
     score: int = 0
+
+
+@dataclass
+class TeamData:
+    id: int = 0
+    oauth_id: int = 0
+    name: str = ""
+    password: str = ""
+    email: str = ""
+    secret: str = ""
+    members: list[UserData] = field(default_factory=list)
+    website: str = ""
+    affiliation: str = ""
+    country: str = ""
+    bracket_id: int = 0
+    hidden: bool = True
+    banned: bool = False
+    captain_id: int = 0
+    captain: UserData = UserData()
+    field_entries: list = field(default_factory=list)
+    created: dt | None = None
